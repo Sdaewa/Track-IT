f195cf0919c6769d67c8d3c2da999bc8d11d6c0e
Luis Ramirez <sdaewa22@gmail.com>
2021-06-25 19:54:21 +0200

    btn in details

--- src/Components/Jobs/FullJobDetails.jsx
+++ src/Components/Jobs/FullJobDetails.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 import classes from "./FullJobDetails.module.css";
 
 const FullJobDetails = (props) => {
   return (
-    <figure className={classes.job}>
-      <p>{props.company}</p>
-      <p>{props.role}</p>
-      <p>{props.techStack}</p>
-      <p>{props.appliedDate}</p>
-    </figure>
+    <Fragment>
+      <figure className={classes.job}>
+        <p>{props.company}</p>
+        <p>{props.role}</p>
+        <p>{props.techStack}</p>
+        <p>{props.appliedDate}</p>
+        <Link className="btn" to={`/jobs`}>
+          Back
+        </Link>
+      </figure>
+    </Fragment>
   );
 };
 
