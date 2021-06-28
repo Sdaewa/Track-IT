a3bf7b360d31639305f2f5ca66fedb7dd74dac71
Luis Ramirez <sdaewa22@gmail.com>
2021-06-28 16:43:40 +0200

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,52 +1,61 @@
-import React, { useContext } from "react";
+import React, { useContext, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 
-import AllJobsPage from "./pages/AllJobsPage";
 import HomePage from "./pages/HomePage";
-import JobDetailsPage from "./pages/JobDetailsPage";
-import NewJobPage from "./pages/NewJobPage";
-import ProfilePage from "./pages/ProfilePage";
-import AuthPage from "./pages/AuthPage";
-import NotFoundPage from "./pages/NotFoundPage";
 import Layout from "./Components/Layout/Layout";
+import LoadingSpinner from "./Components/UI/LoadingSpinner";
 import AuthContext from "./store/auth-context";
+
+const AllJobsPage = React.lazy(() => import("./pages/AllJobsPage"));
+const JobDetailsPage = React.lazy(() => import("./pages/JobDetailsPage"));
+const NewJobPage = React.lazy(() => import("./pages/NewJobPage"));
+const ProfilePage = React.lazy(() => import("./pages/ProfilePage"));
+const AuthPage = React.lazy(() => import("./pages/AuthPage"));
+const NotFoundPage = React.lazy(() => import("./pages/NotFoundPage"));
 
 function App() {
   const authCtx = useContext(AuthContext);
 
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/auth">
-          <AuthPage />
-        </Route>
-        {authCtx.isLoggedIn && (
-          <Route path="/profile">
-            <ProfilePage />
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
           </Route>
-        )}
-        {authCtx.isLoggedIn && (
-          <Route path="/jobs" exact>
-            <AllJobsPage />
+          <Route path="/auth">
+            <AuthPage />
           </Route>
-        )}
-        {authCtx.isLoggedIn && (
-          <Route path="/jobs/:jobId">
-            <JobDetailsPage />
+          {authCtx.isLoggedIn && (
+            <Route path="/profile">
+              <ProfilePage />
+            </Route>
+          )}
+          {authCtx.isLoggedIn && (
+            <Route path="/jobs" exact>
+              <AllJobsPage />
+            </Route>
+          )}
+          {authCtx.isLoggedIn && (
+            <Route path="/jobs/:jobId">
+              <JobDetailsPage />
+            </Route>
+          )}
+          {authCtx.isLoggedIn && (
+            <Route path="/new-job">
+              <NewJobPage />
+            </Route>
+          )}
+          <Route path="*">
+            <NotFoundPage />
           </Route>
-        )}
-        {authCtx.isLoggedIn && (
-          <Route path="/new-job">
-            <NewJobPage />
-          </Route>
-        )}
-        <Route path="*">
-          <NotFoundPage />
-        </Route>
-      </Switch>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
