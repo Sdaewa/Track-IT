2dc0b2977a53ca2ac44685b82c3675389af792a3
Luis Ramirez <sdaewa22@gmail.com>
2021-08-16 18:39:41 +0200

    job listgrid/card

--- src/Components/Jobs/Job.jsx
+++ src/Components/Jobs/Job.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { Delete } from "@material-ui/icons";
+import { Box } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+
+import Card from "@material-ui/core/Card";
+
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 
 import classes from "./Job.module.css";
 
@@ -11,21 +18,39 @@ const Job = (props) => {
   };
 
   return (
-    <li className={classes.job}>
-      <div>
-        <h1>{props.company}</h1>
-      </div>
-      <Button variant="contained" className="btn" href={`/jobs/${props.id}`}>
-        More Details
-      </Button>
-      <Button
-        startIcon={<Delete />}
-        variant="contained"
-        className="btn"
-        onClick={deleteHandler}>
-        Delete
-      </Button>
-    </li>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "auto", padding: "8px" }}>
+      <Card className={classes.root} variant="outlined">
+        <CardContent>
+          <li className={classes.job}>
+            <Box>
+              <h1>{props.company}</h1>
+              <p>{props.appliedDate}</p>
+            </Box>
+          </li>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            className="btn"
+            href={`/jobs/${props.id}`}>
+            Details
+          </Button>
+          <Button
+            startIcon={<Delete />}
+            variant="contained"
+            className="btn"
+            onClick={deleteHandler}>
+            Delete
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 };
 
