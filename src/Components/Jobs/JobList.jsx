07c7213c90258f33768f62803bb115cf18176be4
Luis Ramirez <sdaewa22@gmail.com>
2021-08-17 12:18:10 +0200

    card

--- src/Components/Jobs/JobList.jsx
+++ src/Components/Jobs/JobList.jsx
@@ -4,22 +4,28 @@ import {
   Card,
   CardContent,
   CardActions,
+  CardMedia,
   CssBaseline,
   Grid,
   Container,
+  Button,
+  IconButton,
 } from "@material-ui/core";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 
-import Job from "./Job";
-import classes from "./JobList.module.css";
+// import Job from "./Job";
+// import classes from "./JobList.module.css";
+import useStyles from "./stylesJobList";
 
 const JobList = (props) => {
   // const jobs = props.jobs;
+  const classes = useStyles();
 
   return (
     <>
       <CssBaseline />
       <main>
-        <div>
+        <div className={classes.container}>
           <Container maxWidth="sm">
             <Typography
               variant="h2"
@@ -30,6 +36,42 @@ const JobList = (props) => {
             </Typography>
           </Container>
         </div>
+        <div>
+          <Grid container spacing={0} justifyContent="center">
+            <Grid item>
+              <IconButton className={classes.iconButton} href="/new-job">
+                <AddCircleIcon color="primary" fontSize="medium" />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1966&q=80"
+                  title="Image title"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography variant="h5" gutterBottom>
+                    Job title
+                  </Typography>
+                  <Typography>Date</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button variant="contained" color="primary">
+                    Details
+                  </Button>
+                  <Button variant="contained" color="secondary">
+                    Delete
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
       </main>
     </>
   );
